126b382ddad7b56860d2db522323ef566fa34ec4
amk3y <37068498+amk3y@users.noreply.github.com>
2024-04-26 22:02:12 +0800

    Initial Commit

---
+++ AppleMusicActivity/AppleMusicActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicActivityApp.swift
+//  AppleMusicActivity
+//
+//  Created by amk3y on 2024/4/26.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
